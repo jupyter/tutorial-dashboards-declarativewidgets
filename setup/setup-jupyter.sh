4691400729e6fb649fb8f8c38d786bc8724fa191
Peter Parente <pparent@us.ibm.com>
2016-07-01 08:59:52 -0400

    Pin the dashboards_setup SHA
    (c) Copyright IBM Corp. 2016

--- setup/setup-jupyter.sh
+++ setup/setup-jupyter.sh
@@ -3,10 +3,15 @@
 set -e
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SHA=241fdda2c7ed1497e3b2e8ccff0f0ac134ab1ea8
+PROJECT=dashboards_setup
 
+# Remove existing repo no matter what
+rm -rf $PROJECT
 # Clone dashboards setup repo
-git clone https://github.com/jupyter-incubator/dashboards_setup.git \
-  --depth 1 || true
+git clone https://github.com/jupyter-incubator/${PROJECT}.git
+# Reset to a fixed commit
+git reset --hard $SHA 
   
 pushd dashboards_setup/docker_deploy
 # Fetch pre-assigned credentials from user metadata
